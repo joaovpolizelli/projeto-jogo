1fa83bb87c96a50e9469dbc91b2c494728a3e4de
João Vitor Polizelli <jvitorpzl@gmail.com>
2023-04-20 20:44:09 -0300

    salvando imagens

--- site/index.php
+++ site/index.php
@@ -1,52 +1,66 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>iGames</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-</head>
-<body>
-    
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">iGame</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <!-- <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li> -->
-      </ul>
-      <form action="login.php" method="post" class="d-flex">
-        <input class="form-control me-2" type="email" placeholder="E-mail">
-        <input class="form-control me-2" type="password" placeholder="Password">
-        <button class="btn btn-outline-success me-2" type="submit">Entrar</button>
 
-        <a href="novo-usuario.php" class="btn btn-outline-primary" type="submit">Cadastrar</a>
 
-      </form>
-    </div>
-  </div>
-</nav>    
+<?php
+include "cabecalho.php";
+include "menu.php";
+?>
 
-<div class="container-fluid banner">
-<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+<div id="carouselExampleFade" class="carousel slide carousel-fade">
   <div class="carousel-inner">
     <div class="carousel-item active">
+      <img src="img/dayz-image.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
       <img src="img/ark-image(3).jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="img/rust-image(1).jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="img/dayz-image.jpg" class="d-block w-100" alt="...">
+      <img src="img/redDead-image.jpg" class="d-block w-100" alt="...">
     </div>
   </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-12 text-center"><h1>Jogos em destaque</h1></div>
+    <div class="col-12 text-center"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione nam, sint itaque officiis vitae laboriosam voluptates necessitatibus! Doloribus, consectetur voluptate. Ad temporibus odit porro quam est, consequuntur ratione voluptatibus nulla.</p></div>
+  </div>
+  
+  <div class="row">
+    <div class="col-3 text-center">
+<img src="img/dayz-image.jpg" class="img-fluid" >
+<h2>Nome do Jogo</h2>
+<a href="nomedojogo.php" class="btn btn-primary">VER MAIS!</a>
+    </div>
+
+    <div class="col-3 text-center">
+<img src="img/ark-image(3).jpg" class="img-fluid">
+<h2>Nome do Jogo</h2>
+<a href="nomedojogo.php" class="btn btn-primary">VER MAIS!</a>
+    </div>
+
+    <div class="col-3 text-center">
+<img src="img/redDead-image.jpg" class="img-fluid">
+<h2>Nome do Jogo</h2>
+<a href="nomedojogo.php" class="btn btn-primary">VER MAIS!</a>
+    </div>
+
+    <div class="col-3 text-center">
+<img src="img/dayz-image.jpg" class="img-fluid">
+<h2>Nome do Jogo</h2>
+<a href="nomedojogo.php" class="btn btn-primary">VER MAIS!</a>
+    </div>
+
+
+
+  </div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
