59ae863f63d9c5ad7b7dbcdf5ccf4c3b01841c25
João Vitor Polizelli <jvitorpzl@gmail.com>
2023-05-03 21:16:31 -0300

    criando novo-usuario.php

--- site/login.php
+++ site/login.php
@@ -1,4 +1,6 @@
 <?php
+$email = $_POST["email"];
+$senha = $_POST["senha"];
 
 $servidor_bd = "127.0.0.1";
 $usuario_bd = "root";
@@ -7,9 +9,15 @@ $banco_de_dados = "delivery_jogos_novo";
 
 $conexao = mysqli_connect($servidor_bd, $usuario_bd, $senha_bd, $banco_de_dados);
 
-$sql_buscar_usuario = "select * from usuario";
+$sql_buscar_usuario = "select * from usuario where email = '$email' and senha = '$senha'";
 
-$um_usuario = mysqli_query($conexao, $sql_buscar);
+$um_usuario = mysqli_query($conexao, $sql_buscar_usuario);
+
+if($um_usuario->num_rows > 0){
+    header("location:painel.php");
+}else{
+    header("location:index.php?msg=erro");
+}
 
 mysqli_close($conexao);
 
---
+++ site/novo-usuario.php
@@ -0,0 +1,20 @@
+<?php
+include "cabecalho.php";
+include "menu.php";
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>Cadastrar novo usuário</h1>
+        </div>
+        <div class="row">
+            <div class="col">
+                <form method="post" action="salvar-usuario.php">
+             <input class="mt-2" name="nome" required placeholder="Nome"><br>
+             <input class="mt-2" name="email" type="email" required placeholder="E-mail"><br>
+             <input class="mt-2 name="senha" type="password" required placeholder="Senha"><br>
+             <button type="submit" class="btn btn-success mt-2">Salvar usuário</button>
+                </form>
+            </div>
+        </div>
+    </div>
