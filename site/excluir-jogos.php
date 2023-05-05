23f2885646bc97dc94679248098d7c894ddf05d5
João Vitor Polizelli <jvitorpzl@gmail.com>
2023-05-04 22:22:31 -0300

    Fazendo arquivo de exclusão de jogos, fazendo gerenciados de users e games.

---
+++ site/excluir-jogos.php
@@ -0,0 +1,15 @@
+<?php
+
+$id = $_GET["id"];
+
+include "conexao.php";
+
+$sql_excluir_jogo = "delete from jogo where id = $id";
+
+$um_usuario = mysqli_query($conexao, $sql_excluir_jogo);
+
+mysqli_close($conexao);
+
+header("location:listar-jogos.php?msg=excluido");
+
+?>
